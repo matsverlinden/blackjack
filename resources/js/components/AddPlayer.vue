4d2f48aea9e05acfe62219d561a3c9e838b14a97
Calvin Kleijn <99045362@mydavinci.nl>
2019-12-19 17:08:03 +0100

    Als bezoeker van de website wil ik een naam kunnen opgeven waarmee ik kan spelen
    1. npm install
    2. npm install vue-router vue-axios --save
    3. npm run watch

---
+++ resources/js/components/AddPlayer.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1 class="text-center py-4">Add your name</h1>
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <div class="col-md-6 text-center">
+                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam quas tempore expedita aliquid autem dolorum iure dolores placeat laboriosam! Illum, adipisci quae? Ratione vel sunt corporis deleniti, assumenda repellendus eos.</p><br>
+                <form @submit.prevent="addPlayer">
+                    <div class="form-group">
+                        <input type="text" class="form-control" v-model="player.name">
+                    </div>
+                    <button type="submit" class="btn btn-success">Register</button>
+                </form>
+            </div>
+            <div class="col-sm-3"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                player: {}
+            }
+        },
+        methods: {
+            addPlayer() {
+
+                this.axios
+                    .post('/api/player/add', this.player)
+                    .then(response => (
+                        this.$router.push({name: 'home'})
+                        // console.log(response.data)
+                    ))
+                    .catch(error => console.log(error))
+                    .finally(() => this.loading = false)
+            }
+        }
+    }
+</script>
